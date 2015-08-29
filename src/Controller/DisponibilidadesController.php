7c907f3ba684e818233bcfd971523bdcc73a2742
Kley <wesckley.gomes@gmail.com>
2015-08-29 15:43:08 -0300

    apresentando apenas os titulos

--- src/Controller/DisponibilidadesController.php
+++ src/Controller/DisponibilidadesController.php
@@ -11,6 +11,18 @@ use App\Controller\AppController;
 class DisponibilidadesController extends AppController
 {
 
+    public $paginate = [
+        'fields' => ['Disponibilidades.titulo'],
+        'limit' => 200
+        
+    ];
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+    }
+
     /**
      * Index method
      *
@@ -20,86 +32,5 @@ class DisponibilidadesController extends AppController
     {
         $this->set('disponibilidades', $this->paginate($this->Disponibilidades));
         $this->set('_serialize', ['disponibilidades']);
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id Disponibilidade id.
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $disponibilidade = $this->Disponibilidades->get($id, [
-            'contain' => []
-        ]);
-        $this->set('disponibilidade', $disponibilidade);
-        $this->set('_serialize', ['disponibilidade']);
-    }
-
-    /**
-     * Add method
-     *
-     * @return void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $disponibilidade = $this->Disponibilidades->newEntity();
-        if ($this->request->is('post')) {
-            $disponibilidade = $this->Disponibilidades->patchEntity($disponibilidade, $this->request->data);
-            if ($this->Disponibilidades->save($disponibilidade)) {
-                $this->Flash->success(__('The disponibilidade has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The disponibilidade could not be saved. Please, try again.'));
-            }
-        }
-        $this->set(compact('disponibilidade'));
-        $this->set('_serialize', ['disponibilidade']);
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Disponibilidade id.
-     * @return void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $disponibilidade = $this->Disponibilidades->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $disponibilidade = $this->Disponibilidades->patchEntity($disponibilidade, $this->request->data);
-            if ($this->Disponibilidades->save($disponibilidade)) {
-                $this->Flash->success(__('The disponibilidade has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The disponibilidade could not be saved. Please, try again.'));
-            }
-        }
-        $this->set(compact('disponibilidade'));
-        $this->set('_serialize', ['disponibilidade']);
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Disponibilidade id.
-     * @return void Redirects to index.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $disponibilidade = $this->Disponibilidades->get($id);
-        if ($this->Disponibilidades->delete($disponibilidade)) {
-            $this->Flash->success(__('The disponibilidade has been deleted.'));
-        } else {
-            $this->Flash->error(__('The disponibilidade could not be deleted. Please, try again.'));
-        }
-        return $this->redirect(['action' => 'index']);
     }
 }
