7f0de9d06065f7b7a52b822df98aa87a51085f58
Kley <wesckley.gomes@gmail.com>
2015-09-04 17:42:32 -0300

    retirando ordenacao padrao

--- src/Controller/AlugueisController.php
+++ src/Controller/AlugueisController.php
@@ -29,7 +29,7 @@ class AlugueisController extends AppController
         $alugueis->where(function ($exp, $q) {
                 return $exp->in('situacao', ['R', 'U']);
             });
-        $alugueis->order(['data_inicio' => 'ASC']);
+        //$alugueis->order(['data_inicio' => 'ASC']);
         $alugueis->contain(['Clientes', 'Contas']);
 
         $this->set('alugueis', $this->paginate($alugueis));
