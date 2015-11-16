18a63f8d690b17028ccdee54c218c2e5e2d43987
Onezino Moreira <zinogabriel@gmail.com>
2015-11-16 16:05:53 -0300

    algumas alterações nas senhas expiradas

---
+++ pending.sql
@@ -0,0 +1,17 @@
+/* Nova view de senhas espiradas */
+SELECT  a.id,
+	( SELECT c.email
+            FROM Contas c
+            WHERE c.id = a.conta_id ) email,
+	s.senha,
+	a.data_fim
+FROM Alugueis a, Senhas s
+WHERE s.conta_id = a.conta_id
+  AND a.ativo = 'S'
+  AND s.ativo = 'S'
+  AND s.data_cadastro < a.data_fim
+  AND date_trunc('day', a.data_fim) < NOW()
+
+  /* adiciona coluna de ativo na senha */
+
+  ALTER TABLE Senhas ADD COLUMN ativo character (1) NOT NULL DEFAULT 'A';
