c703af265f6e4444c00f7fe222b7a57995915fd6
Onezino Moreira <zinogabriel@gmail.com>
2015-11-16 15:34:09 -0300

    vamos sugerir algumas senhas :)

--- src/Model/Entity/SenhasExpirada.php
+++ src/Model/Entity/SenhasExpirada.php
@@ -9,4 +9,16 @@ use Cake\ORM\Entity;
 class SenhasExpirada extends Entity
 {
 
+	protected _getNovaSenha()
+	{
+		$chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+	    $length = 8;
+		$len = strlen($chars);
+		$pw = '';
+ 		for ($i=0;$i<$length;$i++)
+        	$pw .= substr($chars, rand(0, $len-1), 1);
+		// the finished password
+		$pw = str_shuffle($pw);
+		return $pw;
+	}
 }
